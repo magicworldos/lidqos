c520e33768704a95972aafc07eb5082829c5a80f
lidq <lidq@bis.com.cn>
2015-03-26 10:42:20 +0800

    .

---
+++ lidqos/include/kernel/kernel.h
@@ -0,0 +1,13 @@
+/*
+ * kernel.h
+ *
+ *  Created on: Mar 26, 2015
+ *      Author: lidq
+ */
+
+#ifndef _INCLUDE_KERNEL_KERNEL_H_
+#define _INCLUDE_KERNEL_KERNEL_H_
+
+int start_kernel(int argc, char **args);
+
+#endif /* INCLUDE_KERNEL_KERNEL_H_ */
---
+++ lidqos/kernel/kernel.c
@@ -0,0 +1,13 @@
+/*
+ * kernel.c
+ *
+ *  Created on: Mar 26, 2015
+ *      Author: lidq
+ */
+
+#include <kernel/kernel.h>
+
+int start_kernel(int argc, char **args)
+{
+	return 0;
+}
