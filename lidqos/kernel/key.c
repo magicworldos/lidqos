e6c80d48c8c324e2b58df86590300ce27085ef9a
lidq <lidq@bis.com.cn>
2015-04-14 10:12:10 +0800

    加入键盘扫描码和可显字符

---
+++ lidqos/kernel/key.c
@@ -0,0 +1,94 @@
+#ifndef _KEY_C_
+#define _KEY_C_
+
+#include<kernel/key.h>
+
+u8 keys[0x53] =
+{
+//
+        0x0,		// ESC
+        '1',		// 1
+        '2',		// 2
+        '3',		// 3
+        '4',		// 4
+        '5',		// 5
+        '6',		// 6
+        '7',		// 7
+        '8',		// 8
+        '9',		// 9
+        '0',		// 0
+        '-',		// -
+        '=',		// =
+        0x0,		// BACKSPACE
+        0x0,		// TAB
+        'Q',		// Q
+        'W',		// W
+        'E',		// E
+        'R',		// R
+        'T',		// T
+        'Y',		// Y
+        'U',		// U
+        'I',		// I
+        'O',		// O
+        'P',		// P
+        '[',		// [
+        ']',		// ]
+        0x0,		// ENTER
+        0x0,		// CTRL
+        'A',		// A
+        'S',		// S
+        'D',		// D
+        'F',		// F
+        'G',		// G
+        'H',		// H
+        'J',		// J
+        'K',		// K
+        'L',		// L
+        ';',		// ;
+        '\'',		// '
+        '`',		// `
+        0x0,		// LEFTSHIFT
+        '\\',		// '\'
+        'Z',		// Z
+        'X',		// X
+        'C',		// C
+        'V',		// V
+        'B',		// B
+        'N',		// N
+        'M',		// M
+        ',',		// ,
+        '.',		// .
+        '/',		// /
+        0x0,		// RIGHTSHIFT
+        0x0,		// PRINTSCREEN
+        0x0,		// ALT
+        0x0,		// SPACE
+        0x0,		// CAPSLOCK
+        0x0,		// F1
+        0x0,		// F2
+        0x0,		// F3
+        0x0,		// F4
+        0x0,		// F5
+        0x0,		// F6
+        0x0,		// F7
+        0x0,		// F8
+        0x0,		// F9
+        0x0,		// F10
+        0x0,		// NUMLOCK
+        0x0,		// SCROLLLOCK
+        0x0,		// HOME
+        0x0,		// UP
+        0x0,		// PAGEUP
+        0x0,		// SUB
+        0x0,		// LEFT
+        0x0,		// CENTER
+        0x0,		// RIGHT
+        0x0,		// ADD
+        0x0,		// END
+        0x0,		// DOWN
+        0x0,		// PAGEDOWN
+        0x0,		// INSERT
+        0x0			// DEL
+        };
+
+#endif
