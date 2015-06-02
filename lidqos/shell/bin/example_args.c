5af229f431443516cda941dc2b030bc2359fb089
lidq <magicworldos@126.com>
2015-06-02 16:17:51 +0800

    shell

--- lidqos/shell/bin/example_args.c
+++ lidqos/shell/bin/example_args.c
@@ -5,15 +5,18 @@
  *      Author: lidq
  */
 
-#include <shell/unistd.h>
 #include <shell/stdio.h>
 
 int main(int argc, char **args)
 {
+	printf("Example args Start:\n");
+
 	for (int i = 0; i < argc; i++)
 	{
 		printf("%d. %s\n", i, args[i]);
 	}
+
+	printf("Example finished.\n");
 
 	return 0;
 }
